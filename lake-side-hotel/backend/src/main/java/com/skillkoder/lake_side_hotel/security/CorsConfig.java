f9290270d4cd0c9759c4b66313a73363ec501c4f
ashutosh7131 <ashutosh713@outlook.com>
2024-12-30 23:19:43 +0800

    bug fix in react and cors issue resolved

--- lake-side-hotel/backend/src/main/java/com/skillkoder/lake_side_hotel/security/CorsConfig.java
+++ lake-side-hotel/backend/src/main/java/com/skillkoder/lake_side_hotel/security/CorsConfig.java
@@ -24,7 +24,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("http://localhost:5175");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
